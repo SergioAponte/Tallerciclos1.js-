80d8858bd6af40b1b894cea3d0b9d5ad86ae2951
SergioAponte <apontelamussergioandres@gmail.com>
2022-12-14 10:30:49 -0500

    tallerciclos

--- tallerciclos.js
+++ tallerciclos.js
@@ -163,13 +163,13 @@
 // console.log(`el sueldo total del vendedor ${nombre} es de ${salario+sueldo+comision}`)
 
 // 10.
-// estudiante=prompt('digite el nombre del estudiante')
+// let estudiante=prompt('digite el nombre del estudiante')
 // while (estudiante!=''){
-//     nota1=parseFloat(prompt('digite la nota 1'))
-//     nota2=parseFloat(prompt('digite la nota 2'))
-//     nota3=parseFloat(prompt('digite la nota 3'))
-//     nota4=parseFloat(prompt('digite la nota 4'))
-//     nota5=parseFloat(prompt('digite la nota 5'))
+//     let nota1=parseFloat(prompt('digite la nota 1'))
+//     let nota2=parseFloat(prompt('digite la nota 2'))
+//     let nota3=parseFloat(prompt('digite la nota 3'))
+//     let nota4=parseFloat(prompt('digite la nota 4'))
+//     let nota5=parseFloat(prompt('digite la nota 5'))
 //     if (nota1 || nota2 || nota3 || nota4 || nota5>=0 && ota1 || nota2 || nota3 || nota4 || nota5<=10){
 //         porcentaje1=((nota1+nota2)/2)*0.4
 //         porcentaje2=((nota3+nota4+nota5)/3)*0.6
